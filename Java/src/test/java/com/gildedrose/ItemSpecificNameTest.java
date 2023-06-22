2856569747f7260d870998ae92ddf133bd35cac5
Dell <Dell@DESKTOP-9T5M8TE>
2023-06-22 05:00:39 -0700

    Refactor : Item name match with given name

--- Java/src/test/java/com/gildedrose/ItemSpecificNameTest.java
+++ Java/src/test/java/com/gildedrose/ItemSpecificNameTest.java
@@ -9,41 +9,64 @@ import com.gildedrose.model.Item;
 import com.gildedrose.service.impl.ItemSpecificNameImpl;
 
 class ItemSpecificNameTest {
-	
+
 	private ItemSpecificNameImpl itemSpecificNameImpl = new ItemSpecificNameImpl();
-	
+
 	@DisplayName("should check Item name not match")
 	@Test
 	void testSpecficItemNamesNotMatch() {
 		Item item = new Item("Test", 2, 2);
 		String itemNameNotMatch = "Age";
 		String itemNameValNotmatch = "Temp";
-		
+
 		Boolean result = itemSpecificNameImpl.isSpecficItemNamesNotMatch(item, itemNameNotMatch, itemNameValNotmatch);
 		assertEquals(Boolean.TRUE, result);
 	}
-	
+
 	@DisplayName("should return true when item name not matched")
 	@Test
 	void testSpecficItemNameNotMatchWithGivenName() {
 		Item item = new Item("Test", 2, 2);
 		String itemNameNotMatch = "Age";
-		
+
 		Boolean result = itemSpecificNameImpl.isSpecficItemNameNotMatchWithGivenName(item, itemNameNotMatch);
 		assertEquals(Boolean.TRUE, result);
-		
+
 	}
-	
+
 	@DisplayName("should return false when item name  matched")
 	@Test
 	void testSpecficItemNameMatchWithGivenNameThenReturnFalse() {
 		Item item = new Item("Test", 2, 2);
 		String itemNameMatch = "Test";
-		
+
 		Boolean result = itemSpecificNameImpl.isSpecficItemNameNotMatchWithGivenName(item, itemNameMatch);
 		assertEquals(Boolean.FALSE, result);
-		
+
+	}
+
+	@DisplayName("should return true when item name  matched")
+	@Test
+	void testSpecficItemNameMatchWithGivenName() {
+
+		Item item = new Item("Test", 2, 2);
+		String itemNameMatch = "Test";
+
+		Boolean result = itemSpecificNameImpl.isSpecficItemNameMatchWithGivenName(item, itemNameMatch);
+		assertEquals(Boolean.TRUE, result);
+
 	}
 	
+	@DisplayName("should return false when item name not matched")
+	@Test
+	void testSpecficItemNameNotMatchWithGivenNameThenReturnFalse() {
+
+		Item item = new Item("Test", 2, 2);
+		String itemNameMatch = "Temp";
+
+		Boolean result = itemSpecificNameImpl.isSpecficItemNameMatchWithGivenName(item, itemNameMatch);
+		assertEquals(Boolean.FALSE, result);
+
+	}
 
 }
